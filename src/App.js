3aa3df170304ae917e4f9c96e461f592b613cdcd
wspawned <wspawned@gmail.com>
2022-08-30 03:27:48 +0300

    css positioning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+
+
 const App = () => {
   const [name,setName] = useState("");
   const [url,setUrl] = useState("");
@@ -9,53 +11,79 @@ const App = () => {
   return (
     <div className="app">
       <header className="app-header">
-        <p>
-          Youtube Raffle
-        </p>
+        <p>Youtube Comment Raffle</p>
       </header>
       <div>
-
         <form className="conditions">
-          <label>Draw Name<input
-          type={"text"}
-          placeholder= "Teddy Picker"
-          value={name}
-          onChange= { (e) => setName(e.target.value) }
-          required
-          ></input></label>
+          <div className="name-url">
+            <label>
+            Draw Name
+              <input
+              type={"text"}
+              placeholder="Teddy Picker"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              ></input>
+            </label>
+
+            <label
+            className="url">
+            Content URL
+              <input
+              type={"url"}
+              placeholder="https://www.youtube.com/watch?v=FwwldUF6TU8"
+              value={url}
+              onChange={(e) => {
+                setUrl(e.target.value);
+
+              }}
+              required
+              ></input>
+            </label>
+          </div>
           
-          <label>Content URL<input
-          type={"url"}
-          placeholder= "https://www.youtube.com/watch?v=FwwldUF6TU8"
-          value= {url}
-          onChange= { (e) => setUrl(e.target.value) }
-          required
-          ></input></label>
-
           <div className="numbers">
-            <label>Principle Winners<input
-            type={"number"}
-            required
-            ></input></label>
+            <label>
+              Winners
+              <input type={"number"} min="1" step="1" required></input>
+            </label>
 
-            <label>Reserve Winners<input
-            type={"number"}
-            required
-            ></input></label>
+            <label>
+              Reserve Winners
+              <input type={"number"} min="1" step="1" required></input>
+            </label>
           </div>
 
-          <label>Keywords<input
-          className="keywords"
-          type={"search"}
-          value={keywords}
-          placeholder= "use space btw keywords"
-          onChange={ (e) => {
-            const words = e.target.value.toLowerCase().split(" ");
-            setKeywords(words);
-          } }
-          ></input></label>
-          
+          <label>
+            Keywords
+            <input
+              className="keywords"
+              type={"search"}
+              value={keywords}
+              placeholder="use space btw keywords"
+              onChange={(e) => {
+                const words = e.target.value.toLowerCase().split(" ");
+                setKeywords(words);
+              }}
+            ></input>
+          </label>
+
+          <button 
+          className="submit"
+          type="submit"> Choose the Winners </button>
+
         </form>
+
+        <div className="rules">
+          <p> Rules of contest </p>
+
+          <div className="buttons">
+            <button> accept repetitive comments as one </button>
+            <button> accept comments of same user as one  </button>
+          </div>
+        </div>
+
       </div>
     </div>
   );
