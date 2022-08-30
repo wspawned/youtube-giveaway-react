d1aa89d8f3a5f58f2bdf5c3a13f3ab761d5776e8
wspawned <wspawned@gmail.com>
2022-08-30 05:44:55 +0300

    api call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { useState } from "react";
 
-
+const API_KEY = "*******************************";
+const API_URL = "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet";
 
 const App = () => {
   const [name,setName] = useState("");
   const [url,setUrl] = useState("");
   const [keywords,setKeywords] = useState([]);
+  const [videoID,setVideoID] = useState("");
+  const [winnerAmount,setWinnerAmount] = useState("");
+  const [reserveWinnerAmount,setReserveWinnerAmount] =useState("");
 
+  async function apiCall() {
+    const res = await fetch(`${API_URL}&videoId=${videoID}&key=${API_KEY}`);
+    const json = await res.json();
+    console.log(json);
+  }
 
   return (
     <div className="app">
@@ -14,7 +23,12 @@ const App = () => {
         <p>Youtube Comment Raffle</p>
       </header>
       <div>
-        <form className="conditions">
+        <form className="conditions"
+        onSubmit={ (e) => {
+          e.preventDefault();
+          apiCall();
+        }}
+        >
           <div className="name-url">
             <label>
             Draw Name
@@ -35,8 +49,10 @@ const App = () => {
               placeholder="https://www.youtube.com/watch?v=FwwldUF6TU8"
               value={url}
               onChange={(e) => {
-                setUrl(e.target.value);
-
+                const value = e.target.value;
+                setUrl(value);
+                const id = value.slice(value.indexOf("=")+1);
+                setVideoID(id);
               }}
               required
               ></input>
@@ -46,12 +62,22 @@ const App = () => {
           <div className="numbers">
             <label>
               Winners
-              <input type={"number"} min="1" step="1" required></input>
+              <input type={"number"} min="1" step="1"
+              value={winnerAmount}
+              onChange= {(e) => {
+                setWinnerAmount(e.target.value);
+              }}
+              required></input>
             </label>
 
             <label>
               Reserve Winners
-              <input type={"number"} min="1" step="1" required></input>
+              <input type={"number"} min="1" step="1"
+              value={reserveWinnerAmount}
+              onChange= { (e) => {
+                setReserveWinnerAmount(e.target.value);
+              }}
+              required></input>
             </label>
           </div>
 
