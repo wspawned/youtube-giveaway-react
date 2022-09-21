ac4503a1bce67ca223734f49034c4580fa383db8
wspawned <wspawned@gmail.com>
2022-09-21 04:43:56 +0300

    Form grids

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { TextField, Button, Typography, Switch } from "@mui/material";
 import { List, ListItem, ListItemText, ListItemAvatar, Avatar } from "@mui/material";
 import { Divider } from "@mui/material";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Paper } from "@mui/material";
+import { fontSize } from "@mui/system";
 
-const API_KEY = "***";
+const API_KEY = "";
 const API_URL = "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet";
 
 const App = () => {
@@ -132,204 +133,241 @@ const App = () => {
   }
 
   return (
-    <Container>
+    <Box
+      sx={{
+        display: "flex",
+        minWidth: "100vw",
+        minHeight: "100vh",
+        p: 3,
+        bgcolor: "primary.light",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <div className="app">
-      <Typography variant="h2" >Youtube Comment Raffle</Typography>
-      <div>
-        <form
-          className="conditions"
-          onSubmit={(e) => {
-            e.preventDefault();
-            apiCall();
-          }}
-        >
-          <TextField
-            type="text"
-            label="Draw Name"
-            placeholder="Spawn Figure Giweavay"
-            variant="outlined"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-          <TextField
-            type="url"
-            label="Content URL"
-            placeholder="https://www.youtube.com/watch?v=FwwldUF6TU8"
-            variant="outlined"
-            value={url}
-            onChange={(e) => {
-              const value = e.target.value;
-              setUrl(value);
-              const id = value.slice(value.indexOf("=") + 1);
-              setVideoID(id);
+        <Typography variant="h3" mb={2} textAlign="center" >
+          Youtube Comment Raffle
+        </Typography>
+        <div>
+          <Paper
+            sx={{
+              width: "100%",
+              p: 2, 
             }}
-            required
-          />
-          <br />
-
-          <TextField
-            type="number"
-            label="Winners"
-            min="1"
-            step="1"
-            variant="outlined"
-            InputLabelProps={{ shrink: true }}
-            value={winnerAmount}
-            onChange={(e) => {
-              setWinnerAmount(e.target.value);
-            }}
-            required
-          />
-          <TextField
-            type="number"
-            label="Reserve Winners"
-            min={winnerAmount}
-            step="1"
-            variant="outlined"
-            value={reserveWinnerAmount}
-            onChange={(e) => {
-              setReserveWinnerAmount(e.target.value);
-            }}
-            required
-          />
-          <br />
-
-          <TextField
-            type="search"
-            label="Keywords"
-            placeholder="McFarlane throne Spawn wings"
-            helperText="use space btw keywords"
-            variant="standard"
-            fullWidth
-            onChange={(e) => {
-              setKeywords(e.target.value);
-            }}
-          />
-          <br />
-
-          <Button type="submit" variant="contained" color="primary">
-            Choose the Winners
-          </Button>
-        </form>
-
-        <div className="rules">
-          <Typography variant="h6">
-            <p> Rules of contest </p>
-            <ul>
-              <li>Reserve winner amount can't be lower than winner amount. </li>
-              <li>
-                If keywords entered picks among the comments that contains all
-                keywords.
-              </li>
-              <li>
-                Accept all comments of same user as one
-                <Switch onClick={() => setUserCondition(!userCondition)} />
-              </li>
-              <li>
-                Accept repetitive comments of same user as one
-                <Switch
-                  onClick={() => setCommentCondition(!commentCondition)}
+            elevation= "24"
+          >
+            <form
+              className="conditions"
+              onSubmit={(e) => {
+                e.preventDefault();
+                apiCall();
+              }}
+            >
+              <Box
+                sx={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(2, 1fr)",
+                  gap:3, p:3,
+                }}
+              >
+                <TextField
+                  type="text"
+                  label="Draw Name"
+                  placeholder="Spawn Figure Giweavay"
+                  variant="outlined"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
                 />
-              </li>
-            </ul>
-          </Typography>
-        </div>
+                <TextField
+                  type="url"
+                  label="Content URL"
+                  placeholder="https://www.youtube.com/watch?v=FwwldUF6TU8"
+                  variant="outlined"
+                  value={url}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setUrl(value);
+                    const id = value.slice(value.indexOf("=") + 1);
+                    setVideoID(id);
+                  }}
+                  required
+                />
 
-        <div className="results">
-          {winners.length ? (
-            <List>
-              {" "}
-              WINNERS
-              {winners.map((winner, order) => {
-                const rank = order + 1;
-                const name = winner.name;
-                const avatar = winner.avatar;
-                const url = winner.url;
-                const text = winner.text;
-                const key = order;
+                <TextField
+                  type="number"
+                  label="Winners"
+                  min="1"
+                  step="1"
+                  variant="outlined"
+                  InputLabelProps={{ shrink: true }}
+                  value={winnerAmount}
+                  onChange={(e) => {
+                    setWinnerAmount(e.target.value);
+                  }}
+                  required
+                />
+                <TextField
+                  type="number"
+                  label="Reserve Winners"
+                  min={winnerAmount}
+                  step="1"
+                  variant="outlined"
+                  value={reserveWinnerAmount}
+                  onChange={(e) => {
+                    setReserveWinnerAmount(e.target.value);
+                  }}
+                  required
+                />
+                </Box>
 
-                return (
-                  <ListItem
-                    alignItems="flex-start"
-                    button
-                    component="a"
-                    href={url}
-                  >
-                    <ListItemAvatar>
-                      <Avatar alt={name} src={avatar} />
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary={` #${rank} ${name} `}
-                      secondary={
-                        <React.Fragment>
-                          <Typography
-                            sx={{ display: "inline" }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                          >
-                            {name}
-                          </Typography>
-                          {` — ${text}`}
-                        </React.Fragment>
-                      }
-                    ></ListItemText>
-                  </ListItem>
-                );
-              })}
-              <Divider variant="inset" component="li" />
-            </List>
-          ) : null}
+                <TextField
+                  type="search"
+                  label="Keywords"
+                  placeholder="McFarlane throne Spawn wings"
+                  helperText="use space btw keywords"
+                  variant="standard"
+                  fullWidth
+                  onChange={(e) => {
+                    setKeywords(e.target.value);
+                  }}
+                  sx={{ width: "60%" }}
+                />
+                <br />
 
-          {reserveWinners.length ? (
-            <List>
-              {" "}
-              RESERVE WINNERS
-              {reserveWinners.map((winner, order) => {
-                const rank = order + 1;
-                const name = winner.name;
-                const avatar = winner.avatar;
-                const url = winner.url;
-                const text = winner.text;
-                const key = order;
+                <Button type="submit" variant="contained" color="primary">
+                  Choose the Winners
+                </Button>
+              
+            </form>
 
-                return (
-                  <ListItem
-                    alignItems="flex-start"
-                    button
-                    component="a"
-                    href={url}
-                  >
-                    <ListItemAvatar>
-                      <Avatar alt={name} src={avatar} />
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary={` #${rank} ${name} `}
-                      secondary={
-                        <React.Fragment>
-                          <Typography
-                            sx={{ display: "inline" }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                          >
-                            {name}
-                          </Typography>
-                          {` — ${text}`}
-                        </React.Fragment>
-                      }
-                    ></ListItemText>
-                  </ListItem>
-                );
-              })}
-            </List>
-          ) : null}
+            <Paper
+              sx={{
+                p: 2,
+                m: 3,
+                bgcolor: "secondary.light",
+                textAlign: "left", 
+              }}
+              
+            >
+              <Typography>
+                <p> Rules of contest </p>
+                <ul>
+                  <li>
+                    Reserve winner amount can't be lower than winner amount.{" "}
+                  </li>
+                  <li>
+                    If keywords entered picks among the comments that contains
+                    all keywords.
+                  </li>
+                  <li>
+                    Accept all comments of same user as one
+                    <Switch onClick={() => setUserCondition(!userCondition)} />
+                  </li>
+                  <li>
+                    Accept repetitive comments of same user as one
+                    <Switch
+                      onClick={() => setCommentCondition(!commentCondition)}
+                    />
+                  </li>
+                </ul>
+              </Typography>
+            </Paper>
+          </Paper>
+
+          <div className="results">
+            {winners.length ? (
+              <List>
+                {" "}
+                WINNERS
+                {winners.map((winner, order) => {
+                  const rank = order + 1;
+                  const name = winner.name;
+                  const avatar = winner.avatar;
+                  const url = winner.url;
+                  const text = winner.text;
+                  const key = order;
+
+                  return (
+                    <ListItem
+                      alignItems="flex-start"
+                      button
+                      component="a"
+                      href={url}
+                    >
+                      <ListItemAvatar>
+                        <Avatar alt={name} src={avatar} />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={` #${rank} ${name} `}
+                        secondary={
+                          <React.Fragment>
+                            <Typography
+                              sx={{ display: "inline" }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              {name}
+                            </Typography>
+                            {` — ${text}`}
+                          </React.Fragment>
+                        }
+                      ></ListItemText>
+                    </ListItem>
+                  );
+                })}
+                <Divider variant="inset" component="li" />
+              </List>
+            ) : null}
+
+            {reserveWinners.length ? (
+              <List>
+                {" "}
+                RESERVE WINNERS
+                {reserveWinners.map((winner, order) => {
+                  const rank = order + 1;
+                  const name = winner.name;
+                  const avatar = winner.avatar;
+                  const url = winner.url;
+                  const text = winner.text;
+                  const key = order;
+
+                  return (
+                    <ListItem
+                      alignItems="flex-start"
+                      button
+                      component="a"
+                      href={url}
+                    >
+                      <ListItemAvatar>
+                        <Avatar alt={name} src={avatar} />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={` #${rank} ${name} `}
+                        secondary={
+                          <React.Fragment>
+                            <Typography
+                              sx={{ display: "inline" }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              {name}
+                            </Typography>
+                            {` — ${text}`}
+                          </React.Fragment>
+                        }
+                      ></ListItemText>
+                    </ListItem>
+                  );
+                })}
+              </List>
+            ) : null}
+          </div>
         </div>
       </div>
-    </div>
-    </Container>
-    
+    </Box>
   );
 };
 
